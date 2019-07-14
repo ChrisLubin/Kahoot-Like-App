adbd626d3060216b264938a902e2c6b8004da422
ChrisLubin <chrisx700@gmail.com>
2019-07-14 16:36:42 -0400

    Client-side game code validation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InitialComponent } from './components/initial/initial.component';
@@ -15,7 +16,8 @@ import { JoinGameComponent } from './components/join-game/join-game.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/components/join-game/join-game.component.ts
+++ client/src/app/components/join-game/join-game.component.ts
@@ -11,17 +11,60 @@ import { fadeInOut } from '../../animations/fadeInOut.animation';
 })
 export class JoinGameComponent implements OnInit {
   private joiningGame: boolean; // True if user attempts to join a game
-  private gameCode: number;
+  private gameCode: string;
   private gameCodeRegex: RegExp;
-  private joinButtonText: string = "Join";
+  private validGameCode: boolean;
+  private invalidText: string;
 
-  ngOnInit() {
+  public ngOnInit(): void {
     this.joiningGame = false;
     this.gameCodeRegex = /^[0-9]*$/;
-    this.joinButtonText = "Join";
+    this.validGameCode = true;
+  }
+
+  public checkInput(): void {
+    // Clears invalid text when input empty
+    if (this.gameCode.length === 0) {
+      this.validGameCode = true;
+      this.invalidText = "";
+      return;
+    }
+
+    // Validation checks
+    if (!this.gameCode.match(this.gameCodeRegex)) {
+      // If game code is not only numbers
+      this.validGameCode = false;
+      this.invalidText = "Numbers only please";
+      return;
+    } else if (this.gameCode.length !== 5) {
+      this.validGameCode = false;
+      this.invalidText = "Code must be 5 integers";
+      return;
+    }
+
+    this.validGameCode = true;
   }
 
   public attemptJoinGame(): void {
+    try {
+      // Validation checks
+      if (!this.validGameCode) {
+        return;
+      } else if (this.gameCode.length <= 0) {
+        // User entered something blank
+        this.validGameCode = false;
+        this.invalidText = "Enter something please";
+        return;
+      }
+    } catch(err) {
+      // User entered something blank (typeof gameCode was undefined)
+      this.validGameCode = false;
+      this.invalidText = "Enter something please";
+      return;
+    }
+
     this.joiningGame = true;
+
+    // Make get request to server...
   }
 }
