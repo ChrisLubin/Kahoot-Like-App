8bd221a965300a9d6ca6207e21dff19de2b06f3e
ChrisLubin <chrisx700@gmail.com>
2019-07-22 20:50:58 -0400

    Added functionality for adding/removing questions in create-game component

--- client/src/app/components/create-game/create-game.component.ts
+++ client/src/app/components/create-game/create-game.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { fadeInOut } from '../../animations/fadeInOut.animation';
 import { Question } from '../../models/question.interface';
+import { CreateQuestionInputs } from '../../models/create-question';
 
 @Component({
   selector: 'app-create-game',
@@ -10,12 +11,71 @@ import { Question } from '../../models/question.interface';
 })
 export class CreateGameComponent implements OnInit {
   private questions: Question[];
+  private inputFields;
   @Output() goBack = new EventEmitter<string>();
 
   constructor() { }
 
   public ngOnInit():void {
-    this.questions = [{ question: "Question 1", choices: [{ choice: 'Choice 1' }, { choice: 'Choice 2' }]}, { question: "Question 2", choices: [{ choice: 'Choice 1' }, { choice: 'Choice 2' }]}]; // Temporary
+    this.inputFields = CreateQuestionInputs;
+    this.questions = [];
+  }
+
+  private addQuestion():void {
+    const valid = this.areInputsValid();
+
+    if (!valid) { return; }
+
+    const question:Question = {
+      question: this.inputFields.question.input,
+      choices: [{
+          choice: this.inputFields.choiceOne.input
+        },
+        {
+          choice: this.inputFields.choiceTwo.input
+        },
+        {
+          choice: this.inputFields.choiceThree.input
+        },
+        {
+          choice: this.inputFields.choiceFour.input
+        }]
+      };
+    this.questions.push(question);
+
+    // Reset input fields
+    for (let key of Object.keys(this.inputFields)) {
+        this.inputFields[key].input = "";
+        this.inputFields[key].valid = true;
+    }
+  }
+
+  private deleteQuestion(questionToDelete: Question):void {
+    this.questions = this.questions.filter(question => question !== questionToDelete);
+  }
+
+  private areInputsValid():boolean {
+    let valid = true;
+
+    // Reset red placeholders
+    for (let key of Object.keys(this.inputFields)) {
+      this.inputFields[key].valid = true;
+    }
+
+    // Validation checks
+    for (let key of Object.keys(this.inputFields)) {
+      if (!this.inputFields[key].input.trim()) {
+        this.inputFields[key].input = "";
+        this.inputFields[key].valid = false;
+        valid = false;
+      }
+    }
+
+    if (valid) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   private goBackToMain():void {
---
+++ client/src/app/models/create-question.ts
@@ -0,0 +1,22 @@
+export const CreateQuestionInputs = {
+    question: {
+      input: <string>"",
+      valid: <boolean>true
+    },
+    choiceOne: {
+      input: <string>"",
+      valid: <boolean>true
+    },
+    choiceTwo: {
+      input: <string>"",
+      valid: <boolean>true
+    },
+    choiceThree: {
+      input: <string>"",
+      valid: <boolean>true
+    },
+    choiceFour: {
+      input: <string>"",
+      valid: <boolean>true
+    }
+  }
